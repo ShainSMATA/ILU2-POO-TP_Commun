0753186b8d72767276674e1ba7cd2e2039a8753b
ShainSMATA <haroun.shain@gmail.com>
2024-11-21 09:45:38 +0100

    oui

---
+++ src/model/CalendrierAnnuel.java
@@ -0,0 +1,23 @@
+package model;
+
+public class CalendrierAnnuel {
+
+	private Mois[] mois;
+	
+	
+	//classe interne mois
+	public static class Mois{
+		
+		public String nom;
+		public boolean [] jour;
+		
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+}
